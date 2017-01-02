5ca1b220cc953fe0bc957cbe36b6b18e377cc95a
Fede Isas <federicoisas@gmail.com>
2017-01-02 14:06:12 -0300

    Remove unused space

--- src/Environment/StdEnvironment.php
+++ src/Environment/StdEnvironment.php
@@ -7,56 +7,56 @@ class StdEnvironment extends Environment
     public function __construct()
     {
         $this->container += [
-            '+' => function () {
+            '+' => function() {
                 return array_sum(func_get_arg(0));
             },
-            '-' => function () {
+            '-' => function() {
                 list($first, $second) = func_get_arg(0); // array [5, 3]
                 return $first - $second;
             },
-            '*' => function () {
+            '*' => function() {
                 return array_product(func_get_arg(0));
             },
-            '/' => function () {
+            '/' => function() {
                 return func_get_arg(0)[0] / func_get_arg(0)[1];
             },
-            '>' => function () {
+            '>' => function() {
                 return func_get_arg(0)[0] > func_get_arg(0)[1];
             },
-            '>=' => function () {
+            '>=' => function() {
                 return func_get_arg(0)[0] >= func_get_arg(0)[1];
             },
-            '<' => function () {
+            '<' => function() {
                 return func_get_arg(0)[0] < func_get_arg(0)[1];
             },
-            '<=' => function () {
+            '<=' => function() {
                 return func_get_arg(0)[0] <= func_get_arg(0)[1];
             },
-            '=' => function () {
+            '=' => function() {
                 return func_get_arg(0)[0] == func_get_arg(0)[1];
             },
-            'abs' => function () {
+            'abs' => function() {
                 return abs(func_get_arg(0));
             },
-            'not' => function () {
+            'not' => function() {
                 return !func_get_arg(0);
             },
-            'first' => function () {
+            'first' => function() {
                 return func_get_arg(0)[0][0];
             },
-            'list' => function () {
+            'list' => function() {
                 return func_get_args()[0];
             },
-            'eq?' => function () {
+            'eq?' => function() {
                 return func_get_arg(0)[0] == func_get_arg(0)[1];
             },
-            'number?' => function () {
+            'number?' => function() {
                 return is_numeric(func_get_arg(0));
             },
-            'quote' => function () {
+            'quote' => function() {
                 return implode(' ', func_get_arg(0));
             },
-            'define' => function () {
+            'define' => function() {
                 list($name, $value) = func_get_arg(0);
                 $this[$name] = $value;
             },
